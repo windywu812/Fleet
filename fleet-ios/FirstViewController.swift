009c2222706594692a1c4037cb3222d70f860803
Muhammad Hilmy Fauzi <fauzi@Fauzis-MacBook-Pro.local>
2020-05-12 11:38:09 +0700

    Initial Commit

---
+++ fleet-ios/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  fleet-ios
+//
+//  Created by Muhammad Hilmy Fauzi on 12/05/20.
+//  Copyright © 2020 Muhammad Hilmy Fauzi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ fleet-ios/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  fleet-ios
+//
+//  Created by Muhammad Hilmy Fauzi on 12/05/20.
+//  Copyright © 2020 Muhammad Hilmy Fauzi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
