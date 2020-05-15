9e7b11609c224eb22138d56c74b2906b32e9984e
Muhammad Hilmy Fauzi <fauzi@Fauzis-MacBook-Pro.local>
2020-05-15 10:25:48 +0700

    create onboarding screen

---
+++ fleet-ios/Views/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  Roundedbut.swift
+//  fleet-ios
+//
+//  Created by Muhammad Hilmy Fauzi on 15/05/20.
+//  Copyright © 2020 Muhammad Hilmy Fauzi. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
