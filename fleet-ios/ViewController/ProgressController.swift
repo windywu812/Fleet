8e29a2c6b0ea1838e3b8277351cb66a8632f5781
Windy <windywu812@gmail.com>
2020-05-19 20:39:27 +0700

    insert chart, but still have some bug

--- fleet-ios/ViewController/ProgressController.swift
+++ fleet-ios/ViewController/ProgressController.swift
@@ -17,11 +17,28 @@ class ProgressController: UIViewController {
     
     let barChart = BarChartView()
     
+    // Dummy data
+    let countSteps = [300, 430, 453, 224, 232, 543, 233]
+    let targetSteps = [500, 500, 500, 500, 1000, 400, 300]
+    
+    let countStepsPerMonth = [1243, 2562, 2323, 3432, 1234, 3455, 2344, 5432, 1243, 4233, 3532, 5432]
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupView()
-
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+//        setChartWeekly(countSteps, targetSteps)
+    }
+    
+    @IBAction func segmentControl(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            setChartWeekly(countSteps, targetSteps)
+        } else if sender.selectedSegmentIndex == 1{
+            setChartAnually(countStepsPerMonth)
+        }
     }
     
     func setupView() {
@@ -32,7 +49,9 @@ class ProgressController: UIViewController {
         
         addShadow(cardView)
         addShadow(chartView)
-                
+        
+        setUpChart()
+                        
     }
     
     func addShadow(_ view: UIView) {
@@ -41,6 +60,120 @@ class ProgressController: UIViewController {
         view.layer.shadowRadius = 20
         view.layer.shadowOpacity = 0.15
         view.layer.shadowOffset = CGSize(width: 0, height: 4)
+    }
+ 
+}
+
+extension ProgressController: ChartViewDelegate {
+    
+    func setChartWeekly(_ countSteps: [Int], _ targetSteps: [Int]) {
+                
+        let dayName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+        
+        var countStepsEntry = [BarChartDataEntry]()
+        var targetStepsEntry = [BarChartDataEntry]()
+        
+        countStepsEntry.removeAll()
+        targetStepsEntry.removeAll()
+    
+        for x in 0..<countSteps.count {
+            // Insert count entry
+            let countStepsData = BarChartDataEntry(x: Double(x), y: Double(countSteps[x]))
+            countStepsEntry.append(countStepsData)
+            
+            // Insert target entry
+            let targetStepsData = BarChartDataEntry(x: Double(x), y: Double(targetSteps[x]))
+            targetStepsEntry.append(targetStepsData)
+        }
+        
+        let countStepsDataSet = BarChartDataSet(entries: countStepsEntry, label: "Total Steps")
+        countStepsDataSet.setColor(.orange)
+        countStepsDataSet.drawValuesEnabled = false
+        
+        let targetStepsDataSet = BarChartDataSet(entries: targetStepsEntry, label: "Your Target")
+        targetStepsDataSet.setColor(.gray)
+        targetStepsDataSet.drawValuesEnabled = false
+        
+        let groupChartData = BarChartData(dataSets: [countStepsDataSet, targetStepsDataSet])
+        groupChartData.notifyDataChanged()
+                
+        // Value
+        let groupSpace = 0.3
+        let barSpace = 0.05
+        let barWidth = 0.4
+        let groupspace = groupChartData.groupWidth(groupSpace: groupSpace, barSpace: barSpace)
+        
+        // XAxis
+        barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: dayName)
+        barChart.xAxis.axisMinimum = 0
+        barChart.xAxis.axisMaximum = groupspace * 7 / 2
+        barChart.xAxis.centerAxisLabelsEnabled = true
+        barChart.xAxis.granularity = 1
+        barChart.xAxis.drawGridLinesEnabled = false
+        
+        groupChartData.barWidth = barWidth
+        groupChartData.groupBars(fromX: 0, groupSpace: groupSpace, barSpace: barSpace)
+        groupChartData.notifyDataChanged()
+        barChart.animate(yAxisDuration: 1, easingOption: .easeInCubic)
+        barChart.data = groupChartData
+        
+    }
+    
+    func setChartAnually(_ countSteps: [Int]) {
+        
+        let monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Des"]
+        
+        var totalStepsEntry = [BarChartDataEntry]()
+        totalStepsEntry.removeAll()
+        
+        for x in 0..<countSteps.count {
+            let totalStepsData = BarChartDataEntry(x: Double(x), y: Double(countSteps[x]))
+            totalStepsEntry.append(totalStepsData)
+        }
+        
+        let countStepsDataSet = BarChartDataSet(entries: totalStepsEntry)
+        countStepsDataSet.setColor(UIColor.orange)
+        countStepsDataSet.drawValuesEnabled = false
+        
+        let chartData = BarChartData(dataSet: countStepsDataSet)
+        chartData.notifyDataChanged()
+
+        barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: monthName)
+        barChart.animate(yAxisDuration: 1, easingOption: .easeInCubic)
+        barChart.data = chartData
+        
+    }
+        
+    func setUpChart() {
+                        
+        barChart.delegate = self
+        barChart.doubleTapToZoomEnabled = false
+        view.addSubview(barChart)
+        
+        barChart.backgroundColor = .clear
+        barChart.isUserInteractionEnabled = false
+        
+        // Grid
+        barChart.rightAxis.enabled = false
+        barChart.xAxis.labelPosition = .bottom
+        barChart.xAxis.drawGridLinesEnabled = false
+        
+        // Legend
+        barChart.legend.horizontalAlignment = .center
+        barChart.legend.enabled = false
+        
+        // Label
+        barChart.leftAxis.labelFont = .systemFont(ofSize: 14)
+        barChart.xAxis.labelFont = .systemFont(ofSize: 14)
+        
+        // Add some constraint
+        barChart.translatesAutoresizingMaskIntoConstraints = false
+        barChart.topAnchor.constraint(equalTo: chartView.topAnchor, constant: 68).isActive = true
+        barChart.bottomAnchor.constraint(equalTo: chartView.bottomAnchor, constant: -16).isActive = true
+        barChart.leadingAnchor.constraint(equalTo: chartView.leadingAnchor, constant: 16).isActive = true
+        barChart.trailingAnchor.constraint(equalTo: chartView.trailingAnchor, constant: -16).isActive = true
+        
+        
     }
     
 }
